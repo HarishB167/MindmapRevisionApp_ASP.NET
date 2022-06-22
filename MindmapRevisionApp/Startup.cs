bfb659601f9f51d91f7f771290657bfad50f2de6
ABC <ABC@ABC-PC>
2022-06-23 03:20:01 +0530

    Add project files.

---
+++ MindmapRevisionApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MindmapRevisionApp.Startup))]
+namespace MindmapRevisionApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
